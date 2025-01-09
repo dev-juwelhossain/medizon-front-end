d76c4c73cc5778cab18fa3d56862dc63c7ba2aa3
wd-ronyroy <ronyroy.cst@gmail.com>
2025-01-09 17:02:00 +0600

    roni's work done

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,12 +1,33 @@
-import './css/Secondary.css'
+import "./css/Secondary.css";
 
 const Doctors = () => {
-    return (
-        <div className='m-2'>
-        {/* Start code here */}
-        <h1>doctors</h1>
-     </div>
-    );
+  return (
+    <div className="m-3">
+      {/* Start code here */}
+      {/* Recommended Doctors List */}
+      <div className="popular-doctors">
+        <h2>জনপ্রিয় ডাক্তাররা</h2>
+      </div>
+      <div className="doctor-profile">
+        <div className="w-100% border  flex  gap-4 rounded-lg shadow-lg p-4 bg-white">
+          <div className="doctor-info">
+            <img src="../img/doctor.jpg" alt="" />
+          </div>
+
+          <div className="doctor-name">
+            <h2>ডাক্তার নাম </h2>
+            <p>ডার্মাটোলজিস্ট (MBBS)</p>
+                <div className="doctor-button bg-blue-500 text-white text-center h-auto w-auto rounded-sm mt-1 ">
+                <p>
+              ডার্মাটোলজিস্ট
+            </p>
+
+                </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Doctors;
