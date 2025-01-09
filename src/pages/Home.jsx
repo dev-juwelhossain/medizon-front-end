53bb13c1dd3f3857cf8df8aa26323de6ba85db0a
unknown <drakj619@gmail.com>
2025-01-09 14:44:14 +0600

    juwel is working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import './css/Primary.css'
+import "./css/Primary.css";
 
 const Home = () => {
   return (
@@ -6,7 +6,7 @@ const Home = () => {
       {/* ################### Start code here ##########################*/}
 
       {/* Intro Video Here */}
-      <div className="intro-video">
+      <div className="intro-video @media screen and (max-width: 800px) {width: 100%;}">
         <iframe
           width="100%"
           height="200px"
@@ -17,6 +17,24 @@ const Home = () => {
           referrerPolicy="strict-origin-when-cross-origin"
           allowfullscreen
         ></iframe>
+      </div>
+      {/* Recommended doctors  */}
+      <div className="recommended-doctors"> 
+        <h2 className="mt-2 ">Recommended Doctors</h2>
+        <div >
+          <h2>সব দেখুন</h2>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#e8eaed"
+            className="float-right text-green-500 rounded-full bg-slate-300" 
+          >
+            <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
+          </svg>
+        </div>
       </div>
     </div>
   );
