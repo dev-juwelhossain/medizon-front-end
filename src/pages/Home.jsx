4d16f914a8b08b9348ff7702c65fbdc6e37fb0f3
unknown <drakj619@gmail.com>
2025-01-11 16:45:20 +0600

    homepage almost done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,15 +21,15 @@ const Home = () => {
 
       {/* Recommended doctors  */}
       <div className="recommended-doctors">
-        <h2 className="mt-2 ">Recommended Doctors</h2>
+        <h2 className="mt-2 text-[13px]">Recommended Doctors</h2>
         <div className="flex items-center justify-between m-2 mt-3">
-          <h2 className="mx-2 ">View All</h2>
+          <h2 className="mx-2 text-[13px] ">View All</h2>
 
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            height="24px"
+            height="20px"
             viewBox="0 -960 960 960"
-            width="24px"
+            width="20px"
             fill="#ffffff"
             className="float-right bg-green-500 rounded-full"
           >
@@ -45,119 +45,123 @@ const Home = () => {
       </section>
       {/* recommended hospitals */}
 
-        <div className="recommended-hospitals">
-          <h2 className="mt-2 ">Recommended Hospitals</h2>
-          <div className="flex items-center justify-between m-2 mt-3 -2">
-            <h2 className="mx-2">View All</h2>
-
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 -960 960 960"
-              width="24px"
-              fill="#ffffff"
-              className="float-right text-green-500 bg-green-500 rounded-full"
-            >
-              <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
-            </svg>
-          </div>
-        </div>
-        {/* Recommended hospitals cards carousel*/}
-        <section className="flex items-center justify-center object-contain">
-          <div className="m-2">
-            {/* here is profile card 1 */}
-
-            <div className="bg-white border border-gray-200 rounded-lg shadow] dark:bg-gray-800 dark:border-gray-700">
-              <a href="#">
-                <img
-                  className="items-center object-cover object-center h-[150px] w-auto"
-                  src="../../img/hospital.jpg"
-                  alt=""
-                />
-              </a>
-              <div className="px-2 py-3">
-                <a href="#">
-                <h2 className="text-[15px] font-bold">রংপুর মেডিকেল কলেজ হাসপাতাল</h2>
-              <p className="text-[13px] m-1 "> +৮৮০-১৩১১২৩৪৫</p>
-              <p className="text-[13px] m-1"><span className="font-bold">ঠিকানা -</span> ধাপ রোড , মেডিকেল মোড়</p>
-                </a>
-                <a
-                  href="#"
-                  className="inline-flex items-center px-2 py-1 font-normal text-center text-white bg-green-500 rounded-lg text-[10] hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-500 dark:hover:bg-green-600 dark:focus:ring-green-800"
-                >
-                  বিস্তারিত
-                  <svg
-                    className="rtl:rotate-180 w-3.5 h-3.5 ms-1"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
-                  </svg>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="m-2">
-            {/* here is profile card 2*/}
-
-            <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <a href="#">
-                <img
-                  className="items-center object-cover object-center h-[150px] w-auto" 
-                  src="../../img/hospital.jpg"
-                  alt=""
-                />
-              </a>
-              <div className="px-2 py-3">
-              <a href="#">
-                <h2 className="text-[18px] font-bold">রংপুর মেডিকেল কলেজ হাসপাতাল</h2>
-              <p className="text-[13px] m-1 "> +৮৮০-১৩XXXXXXXX </p>
-              <p className="text-[13px] m-1"><span className="font-bold">ঠিকানা -</span> ধাপ রোড , মেডিকেল মোড়</p>
-                </a>
-                <a
-                  href="#"
-                 className="inline-flex items-center px-3 py-2 font-medium text-center text-white bg-green-500 rounded-lg flexitems-center hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-500 dark:hover:bg-green-600 dark:focus:ring-green-800 tex-[15px]"
-                >
-                  বিস্তারিত
-                  <svg
-                    className="rtl:rotate-180 w-3.5 h-3.5 ms-1"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
-                  </svg>
-                </a>
-              </div>
-            </div>
-          </div>
-        </section>
-        {/* recommended ambulance */}
-        <div className="recommended-ambulance">
-        <h2 className="mt-2 mb-2">Recommended Ambulance</h2>
-        <div className="flex items-center justify-between m-2 mt-3">
-          <h2 className="mx-2 ">View All</h2>
+      <div className="recommended-hospitals">
+        <h2 className="mt-2 text-[13px]">Recommended Hospitals</h2>
+        <div className="flex items-center justify-between m-2 mt-3 -2">
+          <h2 className="mx-2 text-[13px]">View All</h2>
 
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            height="24px"
+            height="20px"
             viewBox="0 -960 960 960"
-            width="24px"
+            width="20px"
+            fill="#ffffff"
+            className="float-right text-green-500 bg-green-500 rounded-full"
+          >
+            <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
+          </svg>
+        </div>
+      </div>
+      {/* Recommended hospitals cards carousel*/}
+      <section className="flex items-center justify-center object-contain">
+        <div className="m-2">
+          {/* here is profile card 1 */}
+
+          <div className="bg-white border border-gray-200 rounded-lg shadow] dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+              <img
+                className="items-center object-cover object-center h-[150px] w-auto"
+                src="../../img/hospital.jpg"
+                alt=""
+              />
+            </a>
+            <div className="px-2 py-3">
+              <a href="#">
+                <h2 className="text-[14px] font-bold">
+                  রংপুর মেডিকেল কলেজ হাসপাতাল
+                </h2>
+                <p className="text-[13px] m-1 "> +৮৮০-১৩১১২৩৪৫</p>
+                <p className="text-[13px] m-1">ধাপ রোড , মেডিকেল মোড়</p>
+              </a>
+              <a
+                href="#"
+                className="inline-flex items-center px-1 py-1 font-normal text-center text-black  bg-white border border-black rounded-lg text-[13px] hover:bg-green-500 hover:text-white hover:border-white focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-500 dark:hover:bg-green-500 dark:focus:ring-green-800"
+              >
+                বিস্তারিত
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="m-2">
+          {/* here is  profile card 2*/}
+
+          <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+              <img
+                className="items-center object-cover object-center h-[150px] w-auto"
+                src="../../img/hospital.jpg"
+                alt=""
+              />
+            </a>
+            <div className="px-2 py-3">
+              <a href="#">
+                <h2 className="text-[14px] font-bold">
+                  রংপুর মেডিকেল কলেজ হাসপাতাল
+                </h2>
+                <p className="text-[13px] m-1 "> +৮৮০-১৩XXXXXXXX </p>
+                <p className="text-[13px] m-1">ধাপ রোড , মেডিকেল মোড়</p>
+              </a>
+              <a
+                href="#"
+                className="inline-flex items-center px-1 py-1 font-normal text-center text-black  bg-white border border-black rounded-lg text-[13px] hover:bg-green-500 hover:text-white hover:border-white focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-500 dark:hover:bg-green-500 dark:focus:ring-green-800"
+              >
+                বিস্তারিত
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* recommended ambulance */}
+      <div className="recommended-ambulance">
+        <h2 className="mt-2 mb-2 text-[13px]">Recommended Ambulance</h2>
+        <div className="flex items-center justify-between m-2 mt-3">
+          <h2 className="mx-2 text-[13px]">View All</h2>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="20px"
+            viewBox="0 -960 960 960"
+            width="20px"
             fill="#ffffff"
             className="float-right bg-green-500 rounded-full"
           >
@@ -165,92 +169,98 @@ const Home = () => {
           </svg>
         </div>
       </div>
-      
+
       {/* Recommended ambulance cards carousel*/}
       <section className="flex items-center justify-center object-contain">
-          <div className="m-2">
-            {/* here is profile card 1 */}
+        <div className="m-2">
+          {/* here is ambulance profile card 1 */}
 
-            <div className="bg-white border border-gray-200 rounded-lg shadow] dark:bg-gray-800 dark:border-gray-700">
+          <div className="bg-white border border-gray-200 rounded-lg shadow] dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+              <img
+                className="items-center object-cover object-center h-[140px] w-auto"
+                src="../../img/ambulance.jpg"
+                alt=""
+              />
+            </a>
+            <div className="px-2 py-2">
               <a href="#">
-                <img
-                  className="items-center object-cover object-center h-[150px] w-auto"
-                  src="../../img/hospital.jpg"
-                  alt=""
-                />
+                <h2 className="text-[14px] font-bold">মায়ের দোয়া এম্বুলেন্স</h2>
+                <p className="text-[12px] m-1 ">০১৩XXXXXXXX </p>
+                <p className="text-[12px] m-1">ধাপ রোড , মেডিকেল মোড়</p>
               </a>
-              <div className="px-2 py-3">
-                <a href="#">
-                <h2 className="text-[15px] font-bold">রংপুর মেডিকেল কলেজ হাসপাতাল</h2>
-              <p className="text-[10px] m-1 ">০১৩XXXXXXXX </p>
-              <p className="text-[10px] m-1"><span className="font-bold">ঠিকানা -</span> ধাপ রোড , মেডিকেল মোড়</p>
-                </a>
-                <a
-                  href="#"
-                  className="inline-flex items-center px-3 py-2 font-medium text-center text-white bg-green-500 rounded-lg text-[15] hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-500 dark:hover:bg-green-600 dark:focus:ring-green-800"
+              <a
+                href="#"
+                className="inline-flex items-center px-1 py-1 font-normal text-center text-black  bg-white border border-black rounded-lg text-[13px] hover:bg-green-500 hover:text-white hover:border-white focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-500 dark:hover:bg-green-500 dark:focus:ring-green-800"
+              >
+                যোগাযোগ করুন
+                <svg
+                  className="w-5 h-5 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="15px"
+                  height="15px"
+                  fill="none"
+                  viewBox="0 0 24 24"
                 >
-                  বিস্তারিত
-                  <svg
-                    className="rtl:rotate-180 w-3.5 h-3.5 ms-1"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
-                  </svg>
-                </a>
-              </div>
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z"
+                  />
+                </svg>
+              </a>
             </div>
           </div>
-          <div className="m-2">
-            {/* here is profile card 2*/}
+        </div>
+        <div className="m-2">
+          {/* here is ambulance profile card 2*/}
 
-            <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+              <img
+                className="items-center object-cover object-center h-[140px] w-auto"
+                src="../../img/ambulance.jpg"
+                alt=""
+              />
+            </a>
+            <div className="px-2 py-2">
               <a href="#">
-                <img
-                  className="items-center object-cover object-center h-[150px] w-auto" 
-                  src="../../img/hospital.jpg"
-                  alt=""
-                />
+                <h2 className="text-[14px] font-bold">
+                 মায়ের দোয়া এম্বুলেন্স
+                </h2>
+                <p className="text-[13px] m-1 ">০১৩২৩৫৪০৬১</p>
+                <p className="text-[13px] m-1">ধাপ রোড , মেডিকেল মোড়</p>
               </a>
-              <div className="px-2 py-3">
-              <a href="#">
-                <h2 className="text-[18px] font-bold">রংপুর মেডিকেল কলেজ হাসপাতাল</h2>
-              <p className="text-[13px] m-1 "> <span className="font-bold">ফোন নাম্বার -</span> ০১৩XXXXXXXX </p>
-              <p className="text-[13px] m-1"><span className="font-bold">ঠিকানা -</span> ধাপ রোড , মেডিকেল মোড়</p>
-                </a>
-                <a
-                  href="#"
-                 className="inline-flex items-center px-3 py-2 font-medium text-center text-white bg-green-500 rounded-lg flexitems-center hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-500 dark:hover:bg-green-600 dark:focus:ring-green-800 tex-[15px]"
+              <a
+                href="#"
+                className="inline-flex items-center px-1 py-1 font-normal text-center text-black  bg-white border border-black rounded-lg text-[13px] hover:bg-green-500 hover:text-white hover:border-white focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-500 dark:hover:bg-green-500 dark:focus:ring-green-800"
+              >
+                যোগাযোগ করুন
+                <svg
+                  className="w-4 h-5 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="15px"
+                  height="15px"
+                  fill="none"
+                  viewBox="0 0 24 24"
                 >
-                  বিস্তারিত
-                  <svg
-                    className="rtl:rotate-180 w-3.5 h-3.5 ms-1"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 14 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
-                  </svg>
-                </a>
-              </div>
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z"
+                  />
+                </svg>
+              </a>
             </div>
           </div>
-        </section>
+        </div>
+      </section>
     </div>
   );
 };
