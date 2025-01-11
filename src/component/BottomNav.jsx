e933aaa10587f58f2a124135c6308a5a986bf82e
unknown <drakj619@gmail.com>
2025-01-11 23:28:42 +0600

    bottom nav updated

--- src/component/BottomNav.jsx
+++ src/component/BottomNav.jsx
@@ -59,24 +59,19 @@ const BottomNav = () => {
               to="/doctors"
               data-tooltip-target="tooltip-new"
               type="button"
-              className="inline-flex items-center justify-center w-10 h-10 font-medium bg-green-500 rounded-full bg--600 hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
+              className="flex items-center justify-center w-10 h-10 font-normal bg-white bg--600 hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
             >
               <svg
-                className="w-4 h-4 text-white"
-                aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 18 18"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#5f6368"
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 1v16M1 9h16"
-                />
+                <path d="M540-80q-108 0-184-76t-76-184v-23q-86-14-143-80.5T80-600v-240h120v-40h80v160h-80v-40h-40v160q0 66 47 113t113 47q66 0 113-47t47-113v-160h-40v40h-80v-160h80v40h120v240q0 90-57 156.5T360-363v23q0 75 52.5 127.5T540-160q75 0 127.5-52.5T720-340v-67q-35-12-57.5-43T640-520q0-50 35-85t85-35q50 0 85 35t35 85q0 39-22.5 70T800-407v67q0 108-76 184T540-80Zm220-400q17 0 28.5-11.5T800-520q0-17-11.5-28.5T760-560q-17 0-28.5 11.5T720-520q0 17 11.5 28.5T760-480Zm0-40Z" />
               </svg>
-              <span className="sr-only">New item</span>
+
+              <span>New item</span>
             </NavLink>
           </div>
           <div
@@ -138,7 +133,7 @@ const BottomNav = () => {
               />
             </svg>
 
-            <span className="sr-only">Profile</span>
+            <span >More</span>
           </button>
           <div
             id="tooltip-profile"
