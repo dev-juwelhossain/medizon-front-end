e061c3dd3a6bff6b16dfa01d029cfe85bcb7c925
unknown <drakj619@gmail.com>
2025-01-11 12:10:50 +0600

    working on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import "./css/Primary.css";
 
 const Home = () => {
   return (
-    <div className="m-2">
+    <div className="m-2 mb-[270px]">
       {/* ################### Start code here ##########################*/}
 
       {/* Intro Video Here */}
@@ -39,13 +39,12 @@ const Home = () => {
       </div>
       {/* Recommended doctors cards carousel*/}
       <section className="flex items-center justify-between ">
-        <div className="m-5 h-[180px] w-[275px] rounded-2xl shadow-xl">
+        <div className="m-2 h-[180px] w-[275px] rounded-2xl shadow-xl">
           {/* here is profile card */}
         </div>
       </section>
       {/* recommended hospitals */}
 
-      <section>
         <div className="recommended-hospitals">
           <h2 className="mt-2 ">Recommended Hospitals</h2>
           <div className="flex items-center justify-between m-2 mt-3 -2">
@@ -65,12 +64,50 @@ const Home = () => {
         </div>
         {/* Recommended hospitals cards carousel*/}
         <section className="flex items-center justify-between ">
-          <div className="m-5 h-[180px] w-[275px] rounded-2xl shadow-xl">
+          <div className="m-2 h-[180px] rounded-2xl shadow-xl">
             {/* here is profile card */}
 
-            <div className="p-2">
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a href="#">
+                <img
+                  className="rounded-t-lg"
+                  src="../../public/img/hospital.jpg"
+                  alt=""
+                />
+              </a>
+              <div className="p-5">
+                <a href="#">
+                <h2 className="text-lg font-bold">রংপুর মেডিকেল কলেজ হাসপাতাল</h2>
+              <p className="text-[15px]">ফোন নাম্বার - ০১৩XXXXXXXX </p>
+              <p className="text-[15px]">ঠিকানা - ধাপ রোড , মেডিকেল মোড়</p>
+                </a>
+                <a
+                  href="#"
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Read more
+                  <svg
+                    className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+
+            {/* <div className="p-2">
               <img
-                src=".../public/img/hospital.jpg"
+                src="../../public/img/hospital.jpg"
                 alt="Hospital Image"
                 className="rounded-t-2xl"
               />
@@ -93,10 +130,9 @@ const Home = () => {
                   <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z" />
                 </svg>
               </button>
-            </div>
+            </div> */}
           </div>
         </section>
-      </section>
     </div>
   );
 };
