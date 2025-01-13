8445e86fc47771445a0b2ed9a243350833aea1c2
unknown <drakj619@gmail.com>
2025-01-13 13:33:32 +0600

    working

--- src/component/Hospitals.jsx
+++ src/component/Hospitals.jsx
@@ -294,6 +294,7 @@ const Hospitals = () => {
       </div>
       
       {/* marium eye hospital */}
+      
       <div className=" mt-4 mb-20 bg-white border border-gray-200 rounded-lg shadow] dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
           <img
