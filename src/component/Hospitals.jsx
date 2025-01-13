6c41d14d13bd387c97ca3f8e2b8835b8ac70a433
unknown <drakj619@gmail.com>
2025-01-13 13:22:46 +0600

    hospital page done

--- src/component/Hospitals.jsx
+++ src/component/Hospitals.jsx
@@ -224,8 +224,91 @@ const Hospitals = () => {
             <h2 className="text-[15px] font-bold">
             ডক্টরস কমিউনিটি হাসপাতাল (প্রা.) লি.
             </h2>
-            <p className="text-[15px] m-1 "> +৮৮০-১৭৯০৬৩২৩২৩</p>
-            <p className="text-[14px] m-1">মেডিকেল পূর্ব গেট, পূর্ব গেট রংপুর</p>
+            <p className="text-[15px] m-1 "> +৮৮০-১৭০১২৬৪৭০৪ </p>
+            <p className="text-[14px] m-1">মেডিকেল পূর্ব গেট, গংগাচড়া রোড, রংপুর</p>
+          </a>
+          <a
+            href="#"
+            className="flex justify-center w-20 h-6 items-center px-1 py-1 font-normal text-center text-black  bg-white border border-black rounded-lg text-[13px] hover:bg-green-500 hover:text-white hover:border-white   dark:bg-green-500 dark:hover:bg-green-500 dark:focus:ring-green-800"
+          >
+            বিস্তারিত
+            <svg
+              className="rtl:rotate-180 w-3.5 h-3.5 ms-1"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+
+      {/* good health hospitals */}
+      
+      <div className=" mt-4 bg-white border border-gray-200 rounded-lg shadow] dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img
+            className="flex justify-center items-center h-[135px] w-full object-cover rounded-t-lg"
+            src="../../img/good-health.jpg"
+            alt=""
+          />
+        </a>
+        <div className="px-2 py-3">
+          <a href="#">
+            <h2 className="text-[15px] font-bold">
+           গুড হেলথ হাসপাতাল
+            </h2>
+            <p className="text-[15px] m-1 "> +৮৮০-০১৭১৭৯৭৪৪৮৮ </p>
+            <p className="text-[14px] m-1">ধাপ রোড, রংপুর</p>
+          </a>
+          <a
+            href="#"
+            className="flex justify-center w-20 h-6 items-center px-1 py-1 font-normal text-center text-black  bg-white border border-black rounded-lg text-[13px] hover:bg-green-500 hover:text-white hover:border-white   dark:bg-green-500 dark:hover:bg-green-500 dark:focus:ring-green-800"
+          >
+            বিস্তারিত
+            <svg
+              className="rtl:rotate-180 w-3.5 h-3.5 ms-1"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+      
+      {/* marium eye hospital */}
+      <div className=" mt-4 mb-20 bg-white border border-gray-200 rounded-lg shadow] dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img
+            className="flex justify-center items-center h-[135px] w-full object-cover rounded-t-lg"
+            src="../../img/marium.JPG"
+            alt=""
+          />
+        </a>
+        <div className="px-2 py-3">
+          <a href="#">
+            <h2 className="text-[15px] font-bold">
+          মরিয়ম চক্ষু হাসপাতাল
+            </h2>
+            <p className="text-[15px] m-1 "> +৮৮০-০১৭১৬২৯১৭৯৪ </p>
+            <p className="text-[14px] m-1"> জি এল রয় রোড, রেইলগেট, রংপুর</p>
           </a>
           <a
             href="#"
