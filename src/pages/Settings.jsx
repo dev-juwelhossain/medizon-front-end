6643d949df699d8bee5d1be2e2801f31db0e5136
unknown <drakj619@gmail.com>
2025-01-14 16:13:39 +0600

    setting page done

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,10 +1,136 @@
-
 const Settings = () => {
-    return (
-        <div className="m-2 mb-[70px] max-w-md mx-auto">
-            <h1>Settings</h1>
+  return (
+    <div className="m-2 mb-[70px] max-w-md mx-auto">
+    
+      <br />
+      <br />
+      {/* middle section */}
+      <section>
+        {/* settings */}
+        <div className="flex items-center ml-2">
+          <svg
+            className="w-9 h-9"
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#000000"
+          >
+            <path d="m370-80-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm70-80h79l14-106q31-8 57.5-23.5T639-327l99 41 39-68-86-65q5-14 7-29.5t2-31.5q0-16-2-31.5t-7-29.5l86-65-39-68-99 42q-22-23-48.5-38.5T533-694l-13-106h-79l-14 106q-31 8-57.5 23.5T321-633l-99-41-39 68 86 64q-5 15-7 30t-2 32q0 16 2 31t7 30l-86 65 39 68 99-42q22 23 48.5 38.5T427-266l13 106Zm42-180q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 99t99.5 41Zm-2-140Z" />
+          </svg>
+          <h1 className="pl-2 text-[20px] font-medium">Settings (সেটিংস)</h1>
         </div>
-    );
+        {/* help */}
+        <div className="flex items-center mt-2 ml-2">
+          <svg
+            className="mr-2 text-gray-900 h-9 w-9 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9.529 9.988a2.502 2.502 0 1 1 5 .191A2.441 2.441 0 0 1 12 12.582V14m-.01 3.008H12M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+          <h1 className="text-[20px] font-medium">Help (সাহায্য)</h1>
+        </div>
+        {/* feedback */}
+        <div className="flex items-center mt-2 ml-2">
+          <svg
+            className="text-gray-900 w-9 h-9 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M7 11c.889-.086 1.416-.543 2.156-1.057a22.323 22.323 0 0 0 3.958-5.084 1.6 1.6 0 0 1 .582-.628 1.549 1.549 0 0 1 1.466-.087c.205.095.388.233.537.406a1.64 1.64 0 0 1 .384 1.279l-1.388 4.114M7 11H4v6.5A1.5 1.5 0 0 0 5.5 19v0A1.5 1.5 0 0 0 7 17.5V11Zm6.5-1h4.915c.286 0 .372.014.626.15.254.135.472.332.637.572a1.874 1.874 0 0 1 .215 1.673l-2.098 6.4C17.538 19.52 17.368 20 16.12 20c-2.303 0-4.79-.943-6.67-1.475"
+            />
+          </svg>
+          <h1 className="text-[20px] font-medium ml-1">
+            Feedback (ফিডব্যাক)
+          </h1>
+        </div>
+        {/* privacy */}
+        <div className="flex items-center mt-2 ml-2">
+          <svg
+            className="text-gray-900 h-9 w-9 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9.5 11.5 11 13l4-3.5M12 20a16.405 16.405 0 0 1-5.092-5.804A16.694 16.694 0 0 1 5 6.666L12 4l7 2.667a16.695 16.695 0 0 1-1.908 7.529A16.406 16.406 0 0 1 12 20Z"
+            />
+          </svg>
+
+          <h1 className="text-[20px] font-medium ml-2">
+            Privacy (গোপনীয়তা)
+          </h1>
+        </div>
+        
+        {/* about */}
+        <div className="flex items-center mt-2 ml-2">
+          <svg
+            className="text-gray-900 h-9 w-9 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 13V8m0 8h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+          <h1 className="text-[20px] ml-2 font-medium">
+          About (সম্পর্কে)
+          </h1>
+        </div>
+    
+      </section>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div> 
+        <h1 className="text-center text-gray-500">© Medizone - 2025 | V.1.0 </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
